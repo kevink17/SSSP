d3117009dcd420101dc37f7626d48a2192bb6f7d
Kevin <kevink2@gmail.com>
2019-04-21 15:52:43 +0200

    Hab Aufgabe 1 beendet und Aufgabe 2 gemacht

--- Uebung1.c
+++ Uebung1.c
@@ -1,32 +1,67 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct entry{
-	int16_t value;
-	struct entry* left;
-	struct entry* right;
-} Entry;
-
-Entry* grid_init(uint16_t num_columns, uint16_t num_rows)
+//Ein Element einer verketteten Liste die 2 Nachfolger haben kann
+typedef struct element
 {
-	Entry* head = malloc(sizeof(Entry)); //der Kram muss da sein wenn du neuen Entry machst
-	Entry* current = head;
-	//Hier lasse ich die Struktur einer Matrize entstehen 
-	//wie ganz unten auf dem Zettel abgebildet
+	int16_t value;
+	struct element* left;
+	struct element* right;
+} Element;
+/*Erzeugt neues Element wobei die Nachfolger auf NULL zeigen braucht
+  man nachher wenn man die Abfrage bei grid_print macht*/
+Element* new_Element()
+{
+	Element* new = malloc(sizeof(Element));
+	new->right = NULL;
+	new->left = NULL;
+	return new;
+}
+
+Element* grid_init(uint16_t num_columns, uint16_t num_rows)
+{
+	Element* head_element = new_Element();
+	Element* current_row = head_element;
+	Element* current;
+	//Hier lasse ich die Struktur einer Matrize entstehen
 	for(uint16_t j = 0; j < num_rows; j++)
 	{
+		current = current_row;
 		for(uint16_t i = 0;i < num_columns;i++)
 		{
-			current->left = malloc(sizeof(Entry));
-			current = current->left;		
+			/*current.value geht nicht weil current ein pointer auf ein 
+			Element ist, dann muss dan so schreiben mit nem Pfeil ->*/
+			current->value = i+j*num_rows+20; //irgendein Zahlenwert damit man beim printen was sieht
+			current->right = new_Element();
+			current = current->right;
+					
 		}
-		
+		current_row->left = new_Element();
+		current_row = current_row->left;	
 	}
-	return head;
+	return head_element;
+}
+void grid_print(Element* head)
+{
+	Element* current_row = head;
+	Element* current;
+	while(current_row->left != NULL)
+	{
+		printf("| ");
+		current = current_row;
+		while(current->right != NULL)
+		{
+			printf("%i ",current->value);
+			current = current->right;
+		}
+		printf("|\n");
+		current_row = current_row->left;
+	}
 }
 int main()
 {	
-	Entry* head = grid_init(4,1);
-	
+	Element* grid = grid_init(6,8);
+	grid_print(grid);
 	return 0;
 }
+//C ist so ultra gay ich saß da Stunden dran
